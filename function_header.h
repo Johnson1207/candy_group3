5625db8b7f1bbf6fb9ed96060ff7cfa6b7e3db09
Tsai <johnsan11111@gmail.com>
2019-01-13 11:35:55 +0800

    pic

--- function_header.h
+++ function_header.h
@@ -19,7 +19,14 @@ int **create_matrix2(int rows, int cols);
 void printf_matrix2(char *message, int rows, int cols, int **mat);
 void random_mat2D(int **a, int rows, int cols);
 void initialize_mat2D(int **a, int rows, int cols);
-void pic_fill(int **a, int m, int n, ALLEGRO_BITMAP* b, ALLEGRO_BITMAP* b1, ALLEGRO_BITMAP* b2, ALLEGRO_BITMAP* b3, ALLEGRO_BITMAP* b4, ALLEGRO_BITMAP* b5,  ALLEGRO_FONT *pongFont,int score, int step);
+void pic_fill(int **a, int **f, int m, int n,
+              ALLEGRO_BITMAP* b, ALLEGRO_BITMAP* r, ALLEGRO_BITMAP* o, ALLEGRO_BITMAP* p,
+              ALLEGRO_BITMAP* g, ALLEGRO_BITMAP* b5,  ALLEGRO_FONT *pongFont,
+              int score, int step,
+              ALLEGRO_BITMAP* r1, ALLEGRO_BITMAP* r2, ALLEGRO_BITMAP* r3,
+              ALLEGRO_BITMAP* o1, ALLEGRO_BITMAP* o2, ALLEGRO_BITMAP* o3,
+              ALLEGRO_BITMAP* p1, ALLEGRO_BITMAP* p2, ALLEGRO_BITMAP* p3,
+              ALLEGRO_BITMAP* g1, ALLEGRO_BITMAP* g2, ALLEGRO_BITMAP* g3);
 void num_fill(int m, int n, ALLEGRO_FONT *pongFont);
 void free_matrix(int rows, int **mat);
 int **change(int r, int c, int **mat, int **emp, int **fun);
